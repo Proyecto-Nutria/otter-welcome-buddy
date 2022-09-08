358793f3419513d293fca9317118cf65b45c94fe
goberinfinity <reyesfragosoroberto@gmail.com>
2022-09-07 22:25:06 -0700

    Test PR template

--- otter_welcome_buddy/__main__.py
+++ otter_welcome_buddy/__main__.py
@@ -6,6 +6,7 @@ Principal function to be called by Docker
 def main() -> None:
     """Orchestration function"""
     print("Testing GH")
+    print("Testing PR template")
 
 
 if __name__ == "__main__":
