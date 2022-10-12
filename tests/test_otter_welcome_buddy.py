299c18c6bf4f99e886520418c6b09d789da41572
goberinfinity <reyesfragosoroberto@gmail.com>
2022-10-11 21:11:41 -0700

    Add unit test for intent registration

--- tests/test_otter_welcome_buddy.py
+++ tests/test_otter_welcome_buddy.py
@@ -1,5 +1,12 @@
 from otter_welcome_buddy import __version__
+from otter_welcome_buddy.startup import intents
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_getRegisteredIntents_returnMessageNMembers():
+    registered_intents = intents.get_registered_intents()
+    assert registered_intents.messages is True
+    assert registered_intents.members is True
