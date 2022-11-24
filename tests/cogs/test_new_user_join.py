3de98b1a2e8148ac899d47f24409bcce5df7d517
goberinfinity <reyesfragosoroberto@gmail.com>
2022-11-24 14:24:21 -0800

    Move fixture to conftest

--- tests/cogs/test_new_user_join.py
+++ tests/cogs/test_new_user_join.py
@@ -1,28 +1,8 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock
 
 import pytest
 
 from otter_welcome_buddy.cogs import new_user_joins
-
-
-@pytest.fixture
-def mock_bot():
-    mock_bot = AsyncMock()
-    return mock_bot
-
-
-@pytest.fixture
-def mock_msg_fmt():
-    mock_msg_fmt = MagicMock()
-    mock_msg_fmt.bot_is_ready = MagicMock()
-    return mock_msg_fmt
-
-
-@pytest.fixture
-def mock_debug_fmt():
-    mock_debug_fmt = MagicMock()
-    mock_debug_fmt.welcome_message = MagicMock()
-    return mock_debug_fmt
 
 
 @pytest.mark.asyncio
