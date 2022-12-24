62e85e6c2102d3f30199e9a63107b57b9bb6b782
goberinfinity <reyesfragosoroberto@gmail.com>
2022-12-23 20:53:53 -0800

    Add complete path to cogs

--- otter_welcome_buddy/startup/cogs.py
+++ otter_welcome_buddy/startup/cogs.py
@@ -1,5 +1,3 @@
-import logging
-
 from discord.ext.commands import Bot
 
 # from otter_welcome_buddy.cogs import hiring_timelines, new_user_joins
@@ -9,8 +7,8 @@ from otter_welcome_buddy.cogs import new_user_joins
 def __format_module_path_into_cog_extension(absolute_module_path: str) -> str:
     """Transforms absolute module path into <base_path>.<cog_path>.<file>"""
     module_absolute_path_no_extension: str = absolute_module_path[:-3]
-    module_relative_path: list[str] = module_absolute_path_no_extension.split("/")[-2:]
-    return ".".join(module_relative_path)
+    module_full_path: list[str] = module_absolute_path_no_extension.split("/")[-3:]
+    return ".".join(module_full_path)
 
 
 async def register_cogs(bot: Bot) -> None:
@@ -22,8 +20,5 @@ async def register_cogs(bot: Bot) -> None:
 
     for cog in allowed_cogs:
         # __file__ stores absolute path
-        await bot.load_extension(f"otter_welcome_buddy.cogs.new_user_joins")
-        print("Cog loaded")
         cog_extension = __format_module_path_into_cog_extension(cog.__file__)
-        print(cog_extension)
-        # await bot.load_extension(cog_extension)
+        await bot.load_extension(cog_extension)
