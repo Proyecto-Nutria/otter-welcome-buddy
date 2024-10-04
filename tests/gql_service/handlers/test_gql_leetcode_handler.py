a2bf7e7c59d5d802a780f493dee20a5673b6c186
Manuel CH <manuel_7795@hotmail.com>
2024-10-03 18:30:14 -0700

    Type leetcode responses
    Summary:
    
    Test Plan:

--- tests/gql_service/handlers/test_gql_leetcode_handler.py
+++ tests/gql_service/handlers/test_gql_leetcode_handler.py
@@ -5,6 +5,11 @@ import pytest
 
 from otter_welcome_buddy.gql_service.handlers.gql_leetcode_handler import GqlLeetcodeHandler
 from otter_welcome_buddy.gql_service.leetcode_gql_conn import LeetcodeGqlConn
+from otter_welcome_buddy.gql_service.models.gql_leetcode_model import LeetcodeDailyChallengeModel
+from otter_welcome_buddy.gql_service.models.gql_leetcode_model import LeetcodeQuestionModel
+from otter_welcome_buddy.gql_service.models.gql_leetcode_model import LeetcodeTopicTagModel
+from otter_welcome_buddy.gql_service.models.gql_leetcode_model import LeetcodeUserModel
+from otter_welcome_buddy.gql_service.models.gql_leetcode_model import LeetcodeUserProfileModel
 from otter_welcome_buddy.gql_service.query.leetcode_queries import ACTIVE_DAILY_CHALLENGE_QUERY
 from otter_welcome_buddy.gql_service.query.leetcode_queries import USER_PROFILE_QUERY
 
@@ -14,7 +19,10 @@ _USERNAME = "test_user"
 @pytest.mark.asyncio
 async def test_gen_user_public_profile_success() -> None:
     mocked_response = {"matchedUser": {"username": _USERNAME, "profile": {"ranking": 1}}}
-    expected_response = {"username": _USERNAME, "profile": {"ranking": 1}}
+    expected_response = LeetcodeUserModel(
+        username=_USERNAME,
+        profile=LeetcodeUserProfileModel(ranking=1),
+    )
 
     with patch.object(LeetcodeGqlConn, "execute", new_callable=AsyncMock) as mock_execute:
         mock_execute.return_value = mocked_response
@@ -47,19 +55,25 @@ async def test_gen_daily_challenge_success() -> None:
             "question": {
                 "title": "Design a Stack With Increment Operation",
                 "difficulty": "Medium",
+                "categoryTitle": "Algorithms",
                 "topicTags": [{"name": "Array"}, {"name": "Stack"}, {"name": "Design"}],
             },
         },
     }
-    expected_response = {
-        "date": "2024-09-30",
-        "link": "/problems/design-a-stack-with-increment-operation/",
-        "question": {
-            "title": "Design a Stack With Increment Operation",
-            "difficulty": "Medium",
-            "topicTags": [{"name": "Array"}, {"name": "Stack"}, {"name": "Design"}],
-        },
-    }
+    expected_response = LeetcodeDailyChallengeModel(
+        date="2024-09-30",
+        link="/problems/design-a-stack-with-increment-operation/",
+        question=LeetcodeQuestionModel(
+            title="Design a Stack With Increment Operation",
+            difficulty="Medium",
+            category_title="Algorithms",
+            topic_tags=[
+                LeetcodeTopicTagModel(name="Array"),
+                LeetcodeTopicTagModel(name="Stack"),
+                LeetcodeTopicTagModel(name="Design"),
+            ],
+        ),
+    )
 
     with patch.object(LeetcodeGqlConn, "execute", new_callable=AsyncMock) as mock_execute:
         mock_execute.return_value = mocked_response
